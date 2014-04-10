c3d41c8c60343e03fe67735ef72b1693b74ae2a7
Kelsey Brisson <klbrisson@gmail.com>
2014-04-10 14:08:45 -0600

    Created function to make any item editablewith .editable class

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-$(document).on('ready', function() {
-	var displayInputBox = function() {
-		$('.editable').css('display','none');
-		$('.inputText').css('display','block');
-		$('.submit').css('display','block');
-		$('.inputText').focus();
-	};
+var makeEditable = function () {
+	// CREATE INPUT BOX
+	var displayType = $(this).css('display');
+	var inputBox = $("<textarea class='inputBox'>Edit Here</textarea>");
+	$(this).after(inputBox);
+	$('.inputBox').height($(this).outerHeight());
+	$('.inputBox').width($(this).outerWidth());
+	$('.inputBox').css('padding', $(this).css('padding'));
+	$('.inputBox').css('border', $(this).css('border'));
+	
+	//SAVE TEXT AND PRESERVE LINE BREAKS
+	var savedText = $(this).html().replace(/<br>/g, '\n');
+	$('.inputBox').val(savedText);
 
-	$('.editable').click(displayInputBox);
+	//FOCUS AT END
+	$('.inputBox').focus();
+	$('.inputBox').val( $('.inputBox').val() + ' ');
+	
+	//HIDE EDITABLE BOX
+	$(this).css('display','none');
+	var editableItem = this;
 
-	var saveInput = function() {
-		$('.editable').css('display','block');
-		$('.inputText').css('display','none');
-		$('.submit').css('display','none');
-		$('.editable').text($('.inputText').val());
-	};
-	$('.submit').click(saveInput);
-});
+
+	//SAVE INPUT AND REMOVE INPUTBOX ON BLUR
+	inputBox.on('blur', function() {
+		console.log(editableItem);
+
+		//SAVE INPUT TEXT AND PRESERVE LINE BREAKS
+		var inputText = $(this).val().replace(/\n/g, '<br/>');
+		$(editableItem).html(inputText);
+
+		//REMOVE TEXTAREA AND DISPLAY ITEM
+		$(this).remove();
+		$('.editable').css('display', displayType);
+
+	});
+}
+
+$(document).on('click', '.editable', makeEditable);
+
